819a81e72c9a7b5ecb8783b730e369df280a668a
princeassasin <sriramlakshmim@gmail.com>
2017-07-15 19:20:55 +0530

    adding pydev hello

---
+++ hello/hello/__init__.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def __init__(self):
+        print("Hello World")
+
+    def hello(self):
+        print('helloooo')
+        
+hello_world = HelloWorld()
+hello_world.hello()
